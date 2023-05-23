771fc8d46079ac505de4c53082de895383f3940a
Anastasiia Shumeko <vyahirevan@gmail.com>
2023-05-23 21:20:40 +0200

    Added weather API

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+function showTemperature(response) {
+	let temperatureSelector = document.querySelector(".current_temperature");
+	let temperatureNow = Math.round(response.data.main.temp);
+	console.log(temperatureNow);
+	temperatureSelector.innerHTML = temperatureNow;
+}
+
 function formatData(date) {
 	let daysOfWeek = [
 		"Sunday",
@@ -15,13 +22,15 @@ function formatData(date) {
 	return formattedDate;
 }
 
-function changeCity(event) {}
-
 function changeCity(event) {
 	if (event.key === "Enter") {
 		event.preventDefault();
 		let header = document.querySelector("header");
-		header.innerHTML = event.target.value;
+		let city = event.target.value;
+		header.innerHTML = city;
+		let apiKey = "917b5cb46b9991bd0ab660f50601d0c6";
+		let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+		axios.get(apiUrl).then(showTemperature);
 	}
 }
 function convertCelsiusToFahrenheit(celsium) {
